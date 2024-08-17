aaeea78961ebf8a9457ef01d4da5d47ff12c140a
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-08-18 00:26:49 +0530

    Aʅҽxα 👸🏻

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "P6QUBIpS#IGmzhS2DYxXBICbuDVKmtYGDQWjAHUk2pp3gDOyibxk",
 };
