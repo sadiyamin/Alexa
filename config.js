c491f2825bb1fa1b45829f13af644a153a70a045
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-08-21 19:54:18 +0530

    Aʅҽxα 👸🏻

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "P6QUBIpS#IGmzhS2DYxXBICbuDVKmtYGDQWjAHUk2pp3gDOyibxk",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MODE: process.env.MODE || "public",
 };
