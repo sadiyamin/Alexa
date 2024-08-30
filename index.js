1aa6743ac8f50f89f3da20bdb067860729afa07e
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-08-31 01:56:00 +0530

    Aʅҽxα 👸🏻

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const {
     fetchLatestBaileysVersion,
     Browsers
     } = require('@whiskeysockets/baileys')
-    
+
+    const l = console.log
     const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
     const fs = require('fs')
     const P = require('pino')
