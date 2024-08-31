1b7f29284767e5cd74306091f5fef2707e45fc62
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-08-31 10:29:17 +0530

    Aʅҽxα👸🏻

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -1,6 +1,6 @@
+const config = require('../config')
 const fs = require('fs');
 const path = require('path');
-const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 //auto_voice
@@ -12,7 +12,6 @@ async (conn, mek, m, { from, body, isOwner }) => {
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
             if (config.AUTO_VOICE === 'true') {
                 //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
@@ -52,7 +51,8 @@ async (conn, mek, m, { from, body, isOwner }) => {
         if (body.toLowerCase() === text.toLowerCase()) {
             const config = await readEnv();
             if (config.AUTO_REPLY === 'true') {
-                //if (isOwner) return;        
+                //if (isOwner) return;  
+                await conn.sendPresenceUpdate('typing', from);
                 await m.reply(data[text])
             
             }
