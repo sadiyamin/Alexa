f16e86b97328de5ef1bbbec143ba82c9c4f45287
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-10-02 15:00:29 +0530

    Aʅҽxα 👸🏻

--- plugins/alive.js
+++ plugins/alive.js
@@ -7,7 +7,7 @@ cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
-    react: "👧🏻"
+    react: "👧🏻",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
