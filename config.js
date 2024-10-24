3602c37f30dc607c5ef3e2bc4fcb12326b3e2387
sllahiyamd <159688368+sllahiyamd@users.noreply.github.com>
2024-10-24 19:28:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
+2const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MFxQUQSS#E-vrvNCNswnA7JMqbNMxSlTPCs4y_HvAJ7S9xhUCg9E",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
