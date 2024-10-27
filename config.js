16129448befb5316009154302c5c3a4e90095f47
DINETH-XXMD <dinethnimsara112@gmail.com>
2024-10-27 10:43:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PcEggDyR#mkZB3c5igCxg6Y5tYYF5S6Xr7H_H6A_p-J7c7uXg8nI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
