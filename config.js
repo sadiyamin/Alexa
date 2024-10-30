732ffc9bca146fdb09dbbaedc78d92430379f4c3
Qadeer Kulachi <165115669+Qadeerkhan786@users.noreply.github.com>
2024-10-30 05:53:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "B7Fxibqb#8uIIvPv3D42lC96OQ5Prnu1ue3KoQk1in76WL26mYnM",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
