6189b863d03067db672902e3191f733bf553356a
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-11-27 17:40:28 +0530

    Lara-MD

--- config.js
+++ config.js
@@ -5,10 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "arIARDhQ#rOSsDpZmtzYnOBxSrMKbB6gtK9XgjhFAvumVcKmc3fM",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 };
