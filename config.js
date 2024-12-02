7571f931b74af34c7eded7ea156049fee9e85b41
ravidu2004 <sandeepavidu65@gmail.com>
2024-12-03 00:57:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "2mxl1LwA#2c2DcL35hHZSjHOmNyRwFnYOYQP1i9CplreAXD5KBXk", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "flase",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
