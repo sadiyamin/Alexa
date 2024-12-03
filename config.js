28bd74f7afbc996e8582513e89eccaeac3f40054
Venom-md <venomxiter599@gmail.com>
2024-12-03 14:34:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "n2oThbTT#VjouPswMceC0r9gvgEv3M2YxvuFG17KzZ67BbYRfAGQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
