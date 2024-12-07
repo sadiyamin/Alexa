db9811716c1c653b56dc16665af64c17aa028813
Sithara300 <sitharasandeepa300@gmail.com>
2024-12-07 12:02:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "SjA3ELCI#FJol3jN-PYWhogAfL9hk-ppxbWMKRfStN3kAYDsBqVU", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
