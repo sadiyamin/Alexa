d1653d53011bf2dbf995c4a064452c02d1774d37
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-11 20:38:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "b2JyxAoR#mWJbNcuBQpmQr3bOZCHpD7QQ-69QvUsF2pEZJqUvS8k", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "7nxnibxQ#ijGrsJ6lie6MPm4-BQzISZOsM9haTr7II1LfgTiKCM8", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
