9b88a7ac6938ff9bba576f42e9c3a99ef989b522
tharumin <94857655+tharumin@users.noreply.github.com>
2024-12-14 04:52:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
