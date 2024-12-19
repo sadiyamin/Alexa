c529cd8dfdb3bb4d6b03c88fb220ce2a9c7bed4d
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-12-19 22:12:43 +0530

    Lara MD

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "3uRwQKzQ#FtT9xVW77W2TjUfNNqChOce6J3I5N2sKS8R9x999DDM", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
