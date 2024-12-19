3c189d3477494f191174bf2091d92028bbddb3fb
Timothy009504 <kabasatinashe4@gmail.com>
2024-12-19 21:43:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "WuRHlRRD#zOrIES5zXv5zDoxc9zfMX9nEprVnl5Uc45vfmGSqQsg", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
