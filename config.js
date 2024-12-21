11778e6d50f8c4b2b04ae35101458ceda6fb32f0
shaerazmalik7421 <165659336+shaerazmalik7421@users.noreply.github.com>
2024-12-21 18:40:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "Oj4nGAKQ#XLUIL3APRb66IK3guUWPo4NoSCdARk95wMwcfNp6KNw", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
