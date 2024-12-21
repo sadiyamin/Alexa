acf33ac52e751a4c580883469e04cca74732fa42
Dumindu04 <166401628+Dumindu04@users.noreply.github.com>
2024-12-21 08:54:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "bmg2STrK#YYYLfCjfDo-dlVdcK-l3elR83P9E2dH4fmCAkUG-4gs" , // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
