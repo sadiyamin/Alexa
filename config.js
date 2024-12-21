5e57d85366d573ed2bc14ee3bd60fe9b80dfb2a3
Crazy Arrow <yureshkavindu.arrow@hotmail.com>
2024-12-21 18:36:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "Hm4FiKaZ#CRQYruwX2Ut6VWhHRU2yCusYldoNDDhnvSuN5QWHxeI", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
