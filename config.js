4dc506e6040bf37ea7dcce8b0f47e1042215e64f
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-12-22 00:10:39 +0530

    Lara MD

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
